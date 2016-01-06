c36e62a675407648f2b83bf181512b92f74c4d97
Spencer Patty <srobertp@gmail.com>
2016-01-06 17:49:12 -0600

    change the local assembly to use grad X instead of grad chi which is a constant on each cell

--- torus_parametric/local_assembling.m
+++ torus_parametric/local_assembling.m
@@ -29,22 +29,73 @@ function [ local_stiff,local_rhs ] = local_assembling( v,hat_phi,hat_phix,hat_ph
     
     % precompute various things at the quadpoints.
     q_points = [hatx; haty]'*mat_B' + repmat(v(1,:),nq,1);
-    
     rhs_vals = rhs_eval(q_points);
+    
+    % 
+    % X(yhat) = I_h(chi)(yhat) = chi_1 + s(chi_2 - chi_1) + t(chi_3 - chi_1)  [1x3]
+    % 
+    % grad_Ih_chi = grad X(yhat) = [chi_2-chi_1; chi_3- chi_1]'*inv(B)  [3x2]
+    %
+    % G = grad I_h chi * grad I_h chi  % constant matrix on each cell
+    % Q = sqrt(det(Gint) )             % constant on each cell
+    %
+    x = parameterization(v);
+    grad_Ih_chi = [ x(2,:)-x(1,:); x(3,:)-x(1,:)]'*inv_B;
+    G=grad_Ih_chi'*grad_Ih_chi;
+    Q=sqrt(det(G));
+
+%     grad_chi = grad_pm(v);
+%     G=grad_chi'*grad_chi;
+%     Q=sqrt(det(G)); % chi area element
     
     %local stiff and rhs
     for q_index = 1:nq
 %        q_point=AFFINE_MAPPING(v1,v2,v3,[hatx(q_index),haty(q_index)]);
-        grad_chi = grad_pm(q_points(q_index,:));
-        G=grad_chi'*grad_chi;
-        q=sqrt(det(G));
-        invBtgradphi_at_q=inv_B'*[hat_phix(q_index,:); hat_phiy(q_index,:)];
-        local_stiff=local_stiff+ (alpha*invBtgradphi_at_q'*(G\invBtgradphi_at_q)...
-                                  +beta*hat_phi(q_index,:)'*hat_phi(q_index,:))...
-                                  *q_weights(q_index)*det_B*q;
+
+        %
+        % int_{chi(T)} (alpha*surfgrad_phi_i . surfgrad_phi_j + beta*phi_i*phi_j ) d\x
+        %
+        % note:  surfgrad_phi(x) = grad_phistar(y) * inv(G)(y) * grad_chi(y)
+        %
+        % G = grad_chi' grad_chi  ([2x3][3x2])  first fundamental tensor
+        % Q = sqrt(det(G))                      chi map area-element transform
+        %
+        % so \int_{\chi(T)} gradphi_i(x) gradphi_j(x) d\x 
+        %       = ... = \int_{T} grad_phistar_i(y)' * inv(G)(y) * grad_phistar_j(y) * Q dy
+        %
+        %   now mapping to reference element, we have
+        %      grad_phistar(y) = inv(B)*grad_phistar_hat(yhat)
+        %
+        %       = \int_{hat(T)} (inv(B)*grad_phistarhat_i(yhat))' * inv(G)(y) 
+        %                             * (inv(B)*grad_phistar_j(yhat)) * Q * det(B) dyhat
+        %
+        %  
+        %
+        
+%         grad_chi = grad_pm(q_points(q_index,:));
+%         G=grad_chi'*grad_chi;
+%         Q=sqrt(det(G)); % chi area element
+        grad_phi_star_at_q_point=inv_B'*[hat_phix(q_index,:); hat_phiy(q_index,:)]; %grad_phistar
+        
+        grad_phi_ij_matrix = grad_phi_star_at_q_point'*(G\grad_phi_star_at_q_point);
+        phi_ij_matrix = hat_phi(q_index,:)'*hat_phi(q_index,:);
+        
+        local_stiff=local_stiff + (alpha * grad_phi_ij_matrix...
+                                   + beta * phi_ij_matrix )...
+                                  * q_weights(q_index)...
+                                  * det_B...
+                                  * Q;                 
+        %
+        % \int_{chi(T)}  f(x) phi_j(x) d\x
+        %     = \int_{T}  f(x) phistar_j(y) Q(y) dy
+        %         = \int_{hat(T)} f(x) phistar_hat_j(yhat) Q(y) det(B) dyhat
+        %
         if(rhs_flag)
-            local_rhs=local_rhs + rhs_vals(q_index)*hat_phi(q_index,:)'...
-                                   *q_weights(q_index)*det_B*q;
+            local_rhs=local_rhs + rhs_vals(q_index)...    % f(q_point)
+                                  *hat_phi(q_index,:)'... % basis on reference element [3x1]
+                                  *q_weights(q_index)...  % quadrature weight
+                                  *det_B...               % reference area-element transform
+                                  *Q;                     % chi area-element transform
         end
         % The following shows the local assembling in the for loop. We also
         % use the original formula (which can be simplified).
